de996c8b89a531a9a2570049f9200ed71aefa531
km321 <kyle@meltonstudios.com>
2024-06-12 14:56:26 -0400

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
-
 import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://spectrum-transport.com',
   integrations: [tailwind(), react(), prefetch(), sitemap()],
   output: 'hybrid',
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
